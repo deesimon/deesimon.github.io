484cefbdf0ac763a3801ea033aefb37e20cf3b22
slopezdee <slopezdee@varchannelmarketing.com>
2018-01-11 19:44:55 +0800

    Jan 11 added scroll spy and added sponsor images

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-// ------------------------------
-// https://twitter.com/mattsince87
-// ------------------------------
-
-function scrollNav() {
-    $('.nav a').click(function () {
-        //Toggle Class
-        $(".active").removeClass("active");
-        $(this).closest('li').addClass("active");
-        var theClass = $(this).attr("class");
-        $('.' + theClass).parent('li').addClass('active');
+/*function scrollNav() {
+    $('ul.nav li.nav-item a').click(function () {
         //Animate
         $('html, body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 50
@@ -16,11 +7,45 @@ function scrollNav() {
         return false;
 
     });
-    $('.scrollTop a').scrollTop();
 }
-scrollNav();
+scrollNav();*/
 
+
+/*
+function myClass() {
+    $('ul.nav li.nav-item').click(function () {
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.' + theClass).parent('li').removeClass('active');
+        return false;
+
+    });
+}
+myClass();*/
+
+$('#myModal').modal();
 $('a.nav-link ').click(function () {
     //Toggle Class
     $(".collapse").removeClass("show");
+});
+$("ul.nav li a[href^='#']").on('click', function (e) {
+
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // store hash
+    var hash = this.hash;
+
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - 50
+    }, 700, function () {
+
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+    });
+
 });
